0fb454cefb2f9c85397adc842d41fd6a2737bbe2
Sergio Ramirez <sirtdevelopment@gmail.com>
2021-10-07 12:50:35 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,25 @@
+;;; package --- My emac's configuration
+
+;;; Commentary:
+;;; This Emacs configuration uses org mode
+
+;;; Code:
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(setq use-package-always-ensure t)
+
+(use-package org)
+
+(org-babel-load-file "~/.emacs.d/config.org")
+;;; init.el ends here
